ab37723b3517cd5ab8ccfd916281bfe7daa44887
Michelle <michelleyu230@gmail.com>
2021-09-16 21:06:31 -0400

    Created Booking Library

---
+++ BookingLibrary/ApptModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookingLibrary
+{
+    class ApptModel
+    {
+        public PersonModel Person { get; set; }
+        public string ApptTime { get; set; }
+    }
+}
